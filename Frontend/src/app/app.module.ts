765d9fc9aac47b84ecfd6c875df130a710017275
Fady Wafic <fadywafic.123@gmail.com>
2022-10-05 16:24:59 +0200

    add bootstrap and angular material

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+
+let material : any = [
+
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    HttpClientModule,
+    FormsModule,
+    ...material
   ],
   providers: [],
   bootstrap: [AppComponent]
